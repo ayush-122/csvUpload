2c0438ce1f3d309c40d85dbb860161eb5bb63fb3
ayush-122 <ayushsingh4045@gmail.com>
2024-01-04 08:58:31 +0530

    deploy

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /** ------------------ IMPORTING PACKAGE ------------------ **/
 const express = require('express');
-const port = 5000;
+const port = process.env.PORT || 5000;
 const app = express();
 const path = require('path');
 
